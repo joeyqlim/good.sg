e183dc2b6e62cd4ac538cbac53a666637dc34ffa
joeyqlim <joeyqlim@gmail.com>
2020-07-17 14:41:34 +0800

    Created homepage

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 
 router.get("/", (req, res)=>{
   res.render("dashboard/homepage");
-})
+});
+
+router.get("/public-posts", (req, res)=>{
+  res.render("dashboard/public-posts");
+});
 
 module.exports = router;
